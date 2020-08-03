54972f9c3b8b0f2fd6a3a6c7cd831ee542c7f209
Alex Mercer <alexmercerr07@gmail.com>
2020-08-03 22:44:04 +0530

    Moved All the items in Assets Folder

--- js/clock.js
+++
@@ -1,7 +0,0 @@
-const options = {weekday : 'long', year : 'numeric', month : 'long', day : 'numeric'}
-setInterval(() => {
-    var a = new Date
-    tim = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds()
-    day = a.toLocaleDateString(undefined, options);
-    time.innerText = tim + " on " + day  
-}, 1000);
